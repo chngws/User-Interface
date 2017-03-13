2a1cf2476bc8c888d74159bbd4514c67e507c7ed
hafiz703 <mhbt-703@hotmail.com>
2017-03-13 12:17:55 +0800

    Second

---
+++ fiveRows/components/Root.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import Home from './Home'
+export default class fiveRows extends Component {
+  render() {
+    return (
+    <Home />
+    );
+  }
+}
